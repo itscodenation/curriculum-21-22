f1aa07f34bede61ba6b3279210909ac13cefa1ba
David John Coleman II <jcoleman@pagerduty.com>
2022-03-17 16:45:54 -0500

    [Unit 6 Demo] V1: used standard formatting

--- INTRO/units/unit6/demo/index.js
+++ INTRO/units/unit6/demo/index.js
@@ -1,128 +1,89 @@
-$(".yes").click(function(){
+$(".yes").click(function() {
     $(".yes").hide();
     $(".no").hide();
     $(".bakery").hide();
     $("h1").text("Brown or Pink?");
     $(".cake").show();
     $(".bc").show();
-    $(".pc").show(); });
- 
-    $(".no").click(function(){
-      $(".yes").hide();
+    $(".pc").show();
+});
+
+$(".no").click(function() {
+    $(".yes").hide();
     $(".no").hide();
     $(".bakery").hide();
-      $("h1").text("sorry.");
-    
-    });
-$(".bc").click(function(){
-     $(".fork").show();
+    $("h1").text("sorry.");
+});
+
+$(".bc").click(function() {
+    $(".fork").show();
     $(".spoon").show();
     $(".bc").hide();
     $(".pc").hide();
-     $(".cake").hide();
+    $(".cake").hide();
     $("h1").text("Fork or Spoon?");
     $(".cultery").show();
-  
-  });
+});
 
-$(".pc").click(function(){
-     $(".fork").show();
+$(".pc").click(function() {
+    $(".fork").show();
     $(".spoon").show();
     $(".bc").hide();
     $(".pc").hide();
-     $(".cake").hide();
+    $(".cake").hide();
     $("h1").text("Fork or Spoon?");
     $(".cultery").show();
-  
-  });
+});
 
-$(".fork").click(function(){
-      $(".fork").hide();
+$(".fork").click(function() {
+    $(".fork").hide();
     $(".spoon").hide();
-     $(".cultery").hide();
+    $(".cultery").hide();
     $("h1").text("Cake is stuck in your throat!");
     $(".Choke").show();
     $(".Water").show();
-     $(".water").show();
-     });
+    $(".water").show();
+});
 
-$(".spoon").click(function(){
-      $(".fork").hide();
+$(".spoon").click(function() {
+    $(".fork").hide();
     $(".spoon").hide();
-     $(".cultery").hide();
+    $(".cultery").hide();
     $("h1").text("Cake is stuck in your throat!");
     $(".Choke").show();
     $(".Water").show();
-     $(".water").show();
-     });
+    $(".water").show();
+});
 
-$(".Choke").click(function(){
+$(".Choke").click(function() {
     $(".Choke").hide();
     $(".Water").hide();
     $("h1").text("You're Dead. :(");
     $(".die now").show();
     $(".die later").show();
-     $(".water").hide();
-     $(".dead").show();
-       });
+    $(".water").hide();
+    $(".dead").show();
+});
 
-$(".Water").click(function(){
+$(".Water").click(function() {
     $(".Choke").hide();
     $(".Water").hide();
     $("h1").text("It didn't work! What Now!!");
     $(".die").show();
     $(".diee").show();
-       $(".water").hide();
-       });
+    $(".water").hide();
+});
 
-$(".die").click(function(){
-   $(".die").hide();
+$(".die").click(function() {
+    $(".die").hide();
     $(".diee").hide();
-$("h1").text("You're Dead. :(");
-      $(".dead").show();
-       });
-    
-    $(".diee").click(function(){
-   $(".die").hide();
+    $("h1").text("You're Dead. :(");
+    $(".dead").show();
+});
+
+$(".diee").click(function() {
+    $(".die").hide();
     $(".diee").hide();
-$("h1").text("....Now You're Dead. :(");   
-          $(".dead").show();
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $("h1").text("....Now You're Dead. :(");
+    $(".dead").show();
+});
