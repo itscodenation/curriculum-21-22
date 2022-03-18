7263513f0b70b3d845bb1a8ac078ad6834dee40f
David John Coleman II <jcoleman@pagerduty.com>
2022-03-17 23:00:23 -0500

    [Unit 6 Demo] V3: Functional program using data as the source of information

--- INTRO/units/unit6/demo/README.md
+++ INTRO/units/unit6/demo/README.md
@@ -9,3 +9,14 @@ popcode manages automatically such as importing jquery and a stylesheet.
 Other good examples:
 - https://popcode.org/?snapshot=a2e83649-1e18-4c13-baa6-36ebf7d44db6
 - https://popcode.org/?snapshot=8621f042-54ba-4349-875e-1ece22aa4c10
+
+## Versions
+
+- V0: Original Copy
+  - Snapshot: https://popcode.org/?snapshot=fa65b2b1-15e9-4687-b1c9-85d56ca70dca
+- V1: Formatted Code (just click "Format Code" in popcode)
+  - Snapshot: https://popcode.org/?snapshot=761e9c81-0181-428c-a9e4-05abea04addb
+- V2: Use Function to toggle cards
+  - Snapshot: https://popcode.org/?snapshot=227d6064-3d7c-46e8-8771-403fae601c41
+- V3: Functional program using data as the source of information
+  - Snapshot: https://popcode.org/?snapshot=bba56535-0d83-4612-92fb-f062fdaeb5a2
--- INTRO/units/unit6/demo/index.js
+++ INTRO/units/unit6/demo/index.js
@@ -1,32 +1,105 @@
-const hideAllCards = function() {
-    $(".card").hide();
+const cards = {
+    "start": {
+        "cardTitle": "Do you want to go to the Bakery?",
+        "cardImage": "https://media.istockphoto.com/vectors/bakery-shop-building-facade-with-signboard-vector-id1176946222?k=20&m=1176946222&s=612x612&w=0&h=Ss9j_fMfYelT1hBVqQx9a-q9YWIlB3avioxPWSR_IQQ=",
+        "buttons": {
+            "optionOneText": "Yes",
+            "optionTwoText": "No",
+            "optionOneLink": "cake",
+            "optionTwoLink": "end"
+        }
+    },
+    "cake": {
+        "cardTitle": "They're serving Cake, you can afford Brown or Pink?",
+        "cardImage": "https://t3.ftcdn.net/jpg/03/27/07/62/360_F_327076285_G5pC7nCKzueIE4rczZHoNNvSgEfzAhPH.jpg",
+        "buttons": {
+            "optionOneText": "Brown",
+            "optionTwoText": "Pink",
+            "optionOneLink": "cutlery",
+            "optionTwoLink": "cutlery"
+        }
+    },
+    "cutlery": {
+        "cardTitle": "Should you use a Fork or Spoon?",
+        "cardImage": "https://picsum.photos/id/155/400/400",
+        "buttons": {
+            "optionOneText": "Sharp Knife",
+            "optionTwoText": "Spoon",
+            "optionOneLink": "dead",
+            "optionTwoLink": "choke"
+        }
+    },
+    "choke": {
+        "cardTitle": "Cake is stuck in your throat! What should you do?",
+        "cardImage": "https://picsum.photos/id/277/400/400",
+        "buttons": {
+            "optionOneText": "Drink Water",
+            "optionTwoText": "Choke",
+            "optionOneLink": "fail",
+            "optionTwoLink": "dead"
+        }
+    },
+    "fail": {
+        "cardTitle": "It didn't work! What Now!!",
+        "cardImage": "https://picsum.photos/id/300/400/400",
+        "buttons": {
+            "optionOneText": "Keep Drinking!",
+            "optionTwoText": "Jump Outside Window",
+            "optionOneLink": "dead",
+            "optionTwoLink": "window"
+        }
+    },
+    "window": {
+        "cardTitle": "You landed on a rock and the cake popped out",
+        "cardImage": "https://picsum.photos/id/444/400/400",
+        "buttons": {
+            "optionOneText": "Thank the rock",
+            "optionTwoText": "Get more cake",
+            "optionOneLink": "happyEnd",
+            "optionTwoLink": "dead"
+        }
+    },
+    "dead": {
+        "cardTitle": "You're Dead. :(",
+        "cardImage": "https://picsum.photos/id/200/400/400",
+        "buttons": null
+    },
+    "end": {
+        "cardTitle": "Sorry!",
+        "cardImage": "https://picsum.photos/id/134/400/400",
+        "buttons": null
+    },
+    "happyEnd": {
+        "cardTitle": "You Win!",
+        "cardImage": "https://picsum.photos/id/3/400/400",
+        "buttons": null
+    },
 };
 
-const showCard = function(toShow) {
-    hideAllCards();
-    $(toShow).show();
+const showCard = function(cardId) {
+    console.info("invoking showCard() function with cardId argument=" + cardId);
+    let cardData = cards[cardId];
+    let buttons = cardData["buttons"];
+
+    $("#cardTitle").text(cardData["cardTitle"]);
+    $("#cardImage").attr("src", cardData["cardImage"]);
+
+    if (buttons) {
+        $("#optionOne").text(buttons["optionOneText"]);
+        $("#optionOne").off();
+        $("#optionOne").click(function() {
+            showCard(buttons["optionOneLink"]);
+        });
+
+        $("#optionTwo").text(buttons["optionTwoText"]);
+        $("#optionTwo").off();
+        $("#optionTwo").click(function() {
+            showCard(buttons["optionTwoLink"]);
+        });
+    } else {
+        $("#optionOne").hide();
+        $("#optionTwo").hide();
+    }
 };
 
-$(".cake").click(function() {
-    showCard("#cake");
-});
-
-$(".end").click(function() {
-    showCard("#end");
-});
-
-$(".cutlery").click(function() {
-    showCard("#cutlery");
-});
-
-$(".choke").click(function() {
-    showCard("#choke");
-});
-
-$(".dead").click(function() {
-    showCard("#dead");
-});
-
-$(".fail").click(function() {
-    showCard("#fail");
-});
+showCard("start");
