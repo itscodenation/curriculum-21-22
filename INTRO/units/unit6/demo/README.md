ec3da86867a03432703f4c2fa698c77a49b86459
David John Coleman II <jcoleman@pagerduty.com>
2022-03-17 21:56:47 -0500

    [Unit 6 Demo] V2: use function to toggle cards

--- INTRO/units/unit6/demo/README.md
+++ INTRO/units/unit6/demo/README.md
@@ -5,3 +5,7 @@
 This project is taken from popcode here: https://popcode.org/?snapshot=fa65b2b1-15e9-4687-b1c9-85d56ca70dca
 It is designed to be executed in popcode and therefore it is missing some libraries and configuration that
 popcode manages automatically such as importing jquery and a stylesheet.
+
+Other good examples:
+- https://popcode.org/?snapshot=a2e83649-1e18-4c13-baa6-36ebf7d44db6
+- https://popcode.org/?snapshot=8621f042-54ba-4349-875e-1ece22aa4c10
--- INTRO/units/unit6/demo/index.js
+++ INTRO/units/unit6/demo/index.js
@@ -1,89 +1,32 @@
-$(".yes").click(function() {
-    $(".yes").hide();
-    $(".no").hide();
-    $(".bakery").hide();
-    $("h1").text("Brown or Pink?");
-    $(".cake").show();
-    $(".bc").show();
-    $(".pc").show();
+const hideAllCards = function() {
+    $(".card").hide();
+};
+
+const showCard = function(toShow) {
+    hideAllCards();
+    $(toShow).show();
+};
+
+$(".cake").click(function() {
+    showCard("#cake");
 });
 
-$(".no").click(function() {
-    $(".yes").hide();
-    $(".no").hide();
-    $(".bakery").hide();
-    $("h1").text("sorry.");
+$(".end").click(function() {
+    showCard("#end");
 });
 
-$(".bc").click(function() {
-    $(".fork").show();
-    $(".spoon").show();
-    $(".bc").hide();
-    $(".pc").hide();
-    $(".cake").hide();
-    $("h1").text("Fork or Spoon?");
-    $(".cultery").show();
+$(".cutlery").click(function() {
+    showCard("#cutlery");
 });
 
-$(".pc").click(function() {
-    $(".fork").show();
-    $(".spoon").show();
-    $(".bc").hide();
-    $(".pc").hide();
-    $(".cake").hide();
-    $("h1").text("Fork or Spoon?");
-    $(".cultery").show();
+$(".choke").click(function() {
+    showCard("#choke");
 });
 
-$(".fork").click(function() {
-    $(".fork").hide();
-    $(".spoon").hide();
-    $(".cultery").hide();
-    $("h1").text("Cake is stuck in your throat!");
-    $(".Choke").show();
-    $(".Water").show();
-    $(".water").show();
+$(".dead").click(function() {
+    showCard("#dead");
 });
 
-$(".spoon").click(function() {
-    $(".fork").hide();
-    $(".spoon").hide();
-    $(".cultery").hide();
-    $("h1").text("Cake is stuck in your throat!");
-    $(".Choke").show();
-    $(".Water").show();
-    $(".water").show();
-});
-
-$(".Choke").click(function() {
-    $(".Choke").hide();
-    $(".Water").hide();
-    $("h1").text("You're Dead. :(");
-    $(".die now").show();
-    $(".die later").show();
-    $(".water").hide();
-    $(".dead").show();
-});
-
-$(".Water").click(function() {
-    $(".Choke").hide();
-    $(".Water").hide();
-    $("h1").text("It didn't work! What Now!!");
-    $(".die").show();
-    $(".diee").show();
-    $(".water").hide();
-});
-
-$(".die").click(function() {
-    $(".die").hide();
-    $(".diee").hide();
-    $("h1").text("You're Dead. :(");
-    $(".dead").show();
-});
-
-$(".diee").click(function() {
-    $(".die").hide();
-    $(".diee").hide();
-    $("h1").text("....Now You're Dead. :(");
-    $(".dead").show();
+$(".fail").click(function() {
+    showCard("#fail");
 });
